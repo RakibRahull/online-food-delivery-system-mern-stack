062ca833ddea44de087e13041138e693d2249cfe
Rakib <rakibullslamrahul66@gmail.com>
2026-04-12 22:37:37 +0600

    2nd

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
+import genToken from "../utills/token.js"
 
-const signUp=async(req,res)=>{
+export const signUp=async(req,res)=>{
     try {
         const {fullName,email,password,mobile,role}=req.body
         const user=await User.findOne({email})
@@ -22,10 +23,58 @@ const signUp=async(req,res)=>{
             mobile,
             role
         })
-        res.status(201).json({message:"User created successfully", user})
+        const token=await genToken(user._id)
+        res.cookie("token",token,{
+            secure:false,
+            sameSize: "strict",
+            maxAge:7*24*60*60*1000,
+            httpOnly: true
+        })
+
+        return res.status(201).json(user)
+
         
 
     } catch (error) {
-    
+       return res.status(500).json(' sign up error ${error}')
     }
-}    
+} 
+
+export const signIn=async(req,res)=>{
+    try {
+        const {email,password}=req.body
+        const user=await User.findOne({email})
+        if(!user){
+            return res.status(400).json({message:"user does not exist"}) 
+        }
+        const isMatch=await bcrypt.compare(password,user.password)
+        if(!isMatch){
+            return res.status(400).json({message:"incorrect password"})
+        }
+
+        const token=await genToken(user._id)
+        res.cookie("token",token,{
+            secure:false,
+            sameSize: "strict",
+            maxAge:7*24*60*60*1000,
+            httpOnly: true
+        })
+
+        return res.status(201).json(user)
+
+        
+
+    } catch (error) {
+       return res.status(500).json(' sign In error ${error}')
+    }
+} 
+
+export const signOut=async(params)=>{
+    try {
+        res.clearCookie("token")
+
+        return res.status(200).json({message:"sign out successful"})
+    } catch (error) {
+        return res.status(500).json(' sign out error ${error}')
+    }
+}
